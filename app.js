6198d237b466df60df4e81a1c94575a8370ae55c
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-03-15 09:02:27 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+
+//Alternative
+//const app = require('express')
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+
+
+
+
+
+
+app.listen(3000,()=>(
+    console.log("Nodejs server has started at prot 3000")// start the node server at 3000 port
+))
